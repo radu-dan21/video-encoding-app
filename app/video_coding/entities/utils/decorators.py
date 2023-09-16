f7ec71cce116029bc0b642499744eca331b6cc41
Radu-Anton Dan <80848522+radu-dan21@users.noreply.github.com>
2023-09-16 17:32:48 +0300

    ignore_errors decorator refactor + docstring

--- app/video_coding/entities/utils/decorators.py
+++ app/video_coding/entities/utils/decorators.py
@@ -3,7 +3,13 @@ from functools import wraps
 from typing import Any
 
 
-def ignore_errors(err_list: list[Exception] = None) -> Callable:
+def ignore_errors(err_list: list[Exception] = None, default=None) -> Callable:
+    """
+    Decorator used for ignoring errors
+    Returns a default value when an "allowed" exception is raised
+    :param err_list: list of allowed exceptions
+    :param default: default value to be returned
+    """
     def decorator_ignore_errors(func) -> Callable:
         @wraps(func)
         def wrapper_ignore_errors(*args, **kwargs) -> Any:
@@ -11,7 +17,7 @@ def ignore_errors(err_list: list[Exception] = None) -> Callable:
                 return func(*args, **kwargs)
             except Exception as e:
                 if err_list and any(isinstance(e, err_type) for err_type in err_list):
-                    return None
+                    return default
                 raise e
 
         return wrapper_ignore_errors
