875ade4df40c2ee162f74d6c53ab935ae024e995
Radu-Anton Dan <80848522+radu-dan21@users.noreply.github.com>
2023-10-08 14:17:11 +0300

    ci plan - pre-commit hooks

--- scripts/run_code_checks.sh
+++ scripts/run_code_checks.sh
@@ -6,5 +6,10 @@ PROJECT_ROOT=$(dirname "$SCRIPT_DIR")
 
 cd "$PROJECT_ROOT" || exit
 
-docker build . --file="./Dockerfile_precommit" --tag="code_checks:latest"
-docker run --rm -v "$PWD":/project code_checks:latest bash -c "pre-commit run --a $*"
+if [[ -z ${CODE_CHECKS_IMAGE_TAG} ]]; then
+  # local run, code checks image must be built
+  CODE_CHECKS_IMAGE_TAG='code_checks:latest'
+  docker build . --file="./Dockerfile_precommit" --tag=$CODE_CHECKS_IMAGE_TAG
+fi
+
+docker run --rm -v "$PWD":/project $CODE_CHECKS_IMAGE_TAG bash -c "pre-commit run --a $*"
